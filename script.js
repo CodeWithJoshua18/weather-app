5ba302961e642ed719bba8f5ca8796162dc693bf
CodeWithJoshua18 <generaljosh5311@gmail.com>
2026-02-19 23:09:38 +0300

    add DOM selectors and initial structure for weather app functionality

---
+++ script.js
@@ -0,0 +1,36 @@
+// DOM SELECTORS
+const form = document.querySelector('#weather-form');
+const locationInput = document.getElementById('location-input');
+const loadingSection = document.getElementById('loading');
+const weatherDisplay = document.getElementById('weather-dispaly');
+const errorSection = document.getElementById('error-message');
+
+// WEATHER DISPLAY
+const cityName = document.getElementById('city-name');
+const temperature = document.getElementById('temperature');
+const description = document.getElementById('description');
+const humidity = document.getElementById('humidity');
+const windSpeed = document.getElementById('wind-speed');
+const weatherIcon = document.getElementById('weather-icon')
+
+const API_KEY = 
+
+// API FUNCTIONS
+async function fetchWeather(location){
+    const response = await fetch()
+}
+
+
+// DATA PROCESSING
+function processWeatherData(data){
+
+}
+
+
+// UI / DOM MANIPULATION
+
+
+// CONTROLLER / EVENT LISTENERS
+form.addEventListener('submit', async (e) => {
+
+})
